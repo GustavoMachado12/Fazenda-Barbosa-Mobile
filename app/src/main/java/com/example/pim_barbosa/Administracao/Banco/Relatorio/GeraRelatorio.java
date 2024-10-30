26a12254e1b22d957b98c146a86e53d94c874402
Gustavo Soares <gustavomachado104@gmail.com>
2024-10-30 17:49:19 -0300

    Feito o primeiro relatório: Total Vendas

--- app/src/main/java/com/example/pim_barbosa/Administracao/Banco/Relatorio/GeraRelatorio.java
+++ app/src/main/java/com/example/pim_barbosa/Administracao/Banco/Relatorio/GeraRelatorio.java
@@ -1,19 +1,49 @@
 package com.example.pim_barbosa.Administracao.Banco.Relatorio;
 
+import android.app.Activity;
+import android.content.Context;
+import android.widget.TextView;
+
+import com.example.pim_barbosa.Conexao.ConSQL;
+import com.example.pim_barbosa.R;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GeraRelatorio {
     Connection connection;
     String ConnectionResult;
 
     //TOTAL VENDAS
-//    public int totalVendas(){
-//        int total;
-//
-//
-//
-//        return total;
-//    }
+    public void totalVendas(int ano, Context context){
+        try {
+            ConSQL sql = new ConSQL();
+            connection = sql.con();
+            if (connection != null) {
+                String qu = "SELECT * FROM vTotalVendas WHERE ANO = " + ano;
+                Statement st = connection.createStatement();
+                ResultSet rs = st.executeQuery(qu);
+
+                while (rs.next()){
+                    int totalVendas = rs.getInt("TOTAL");
+                    TextView txtTotalVendas = ((Activity) context).findViewById(R.id.txtTotalVendas_QuantidadeTotal_Relatorio);
+                    txtTotalVendas.setText(String.valueOf(totalVendas));
+                    TextView txtAno = ((Activity) context).findViewById(R.id.txtTotalVendas_Ano_Relatorio);
+                    txtAno.setText(String.valueOf(ano));
+                }
+
+                ConnectionResult = "Success";
+                connection.close();
+            } else {
+                ConnectionResult = "Failed";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/example/pim_barbosa/Administracao/Banco/Relatorio/RelatorioConsulta.java
+++ app/src/main/java/com/example/pim_barbosa/Administracao/Banco/Relatorio/RelatorioConsulta.java
@@ -12,6 +12,11 @@ import com.example.pim_barbosa.R;
 public class RelatorioConsulta extends AppCompatActivity {
     private TextView textNome, textCargo, textConsultaOpcao;
 
+    //TOTAL DE VENDAS
+    private TextView txtTotalVendas_Relatorio, txtTotalVendas_Ano_Relatorio, txtTotalVendas_QuantidadeTotal_Relatorio;
+
+    GeraRelatorio geraRelatorio = new GeraRelatorio();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,16 @@ public class RelatorioConsulta extends AppCompatActivity {
         textCargo = findViewById(R.id.textCargoDash);
         textNome.setText(getIntent().getStringExtra("Nome"));
         textCargo.setText(getIntent().getStringExtra("Cargo"));
+
+        //TOTAL DE VENDAS
+        txtTotalVendas_Relatorio = findViewById(R.id.txtTotalVendas_Relatorio);
+        txtTotalVendas_Ano_Relatorio = findViewById(R.id.txtTotalVendas_Ano_Relatorio);
+        txtTotalVendas_QuantidadeTotal_Relatorio = findViewById(R.id.txtTotalVendas_QuantidadeTotal_Relatorio);
+
+        geraRelatorio.totalVendas(2024, RelatorioConsulta.this);
+
+
+
 
         textConsultaOpcao = findViewById(R.id.textDashboardRelatorio_Consulta);
         textConsultaOpcao.setOnClickListener(new View.OnClickListener() {
